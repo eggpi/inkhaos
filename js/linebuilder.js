5b49f14496c2b5615be424517c39af6abdbe7b43
Guilherme Gonçalves <guilherme.p.gonc@gmail.com>
2012-09-19 19:57:39 -0300

    New calculation of line polygons.
    Still very buggy.

--- js/linebuilder.js
+++ js/linebuilder.js
@@ -9,18 +9,23 @@ function LineBuilder() {
   this.lineTo = function(x, y, width) {
     // TODO: generate less polygons?
     // TODO: stitch polygons together with small rectangles?
+    //
+
+    var dx = (x - lastX), dy = (y - lastY);
+    var m = dy/dx, theta = Math.atan(m);
+    var sint = Math.sin(theta), cost = Math.cos(theta);
+
     var vertices = [
+      [x + width*sint/2, y - width*cost/2],
+      [lastX + width*sint/2, lastY - width*cost/2],
       [lastX, lastY],
-      [x, y],
-      [lastX, lastY + width],
-      [x, y + width]
+      [lastX - width*sint/2, lastY + width*cost/2],
+      [x - width*sint/2, y + width*cost/2],
+      [x, y]
     ];
 
     // CraftyBox expects the position of the center.
-    var posX = (x - lastX)/2;
-    var posY = (y - lastY)/2;
-
-    Crafty.e("Box2D").attr({x: posX, y: posY}).polygon(vertices);
+    Crafty.e("Box2D").attr({x: dx/2, y: dy/2}).polygon(vertices);
 
     lastX = x;
     lastY = y;
