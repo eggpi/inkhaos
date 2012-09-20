3ca54b4c402e010f441d96bd2eeb0843278afb24
Guilherme Gonçalves <guilherme.p.gonc@gmail.com>
2012-09-20 20:09:17 -0300

    Working version of canvas -> rectangles transform.

--- js/linebuilder.js
+++ js/linebuilder.js
@@ -6,23 +6,57 @@ function LineBuilder() {
     lastY = y;
   }
 
-  this.lineTo = function(x, y, width) {
-    // TODO: generate less polygons?
-    // TODO: stitch polygons together with small rectangles?
-    //
+  /*
+   * Sort an array of vertices counterclockwise relative to the
+   * baricenter of the polygon they determine.
+   */
+  function sortVerticesCCW(vertices) {
+    var center = {x: 0, y: 0};
+    for (i = 0; i < vertices.length; i++) {
+      center.x += vertices[i][0];
+      center.y += vertices[i][1];
+    }
 
-    var dx = (x - lastX), dy = (y - lastY);
-    var m = dy/dx, theta = Math.atan(m);
-    var sint = Math.sin(theta), cost = Math.cos(theta);
+    center.x /= vertices.length;
+    center.y /= vertices.length;
+
+    vertices.sort(function(v1, v2) {
+      var a1 = Math.atan2(v1[1] - center.y, v1[0] - center.x);
+      var a2 = Math.atan2(v2[1] - center.y, v2[0] - center.x);
+
+      return a1 - a2;
+    });
+  }
+
+  /*
+   * Build an array of vertices for a line interpolation
+   * rectangle for (lastX, lastY) and (x, y).
+   * Returns the array of vertices sorted counterclockwise relative
+   * to the rectangle's baricenter.
+   */
+  function buildRectangleVertices(x, y, width) {
+    var m = (y - lastY) / (x - lastX),
+        theta = Math.atan(m),
+        sint = Math.sin(theta),
+        cost = Math.cos(theta);
 
     var vertices = [
       [x + width*sint/2, y - width*cost/2],
       [lastX + width*sint/2, lastY - width*cost/2],
-      [lastX, lastY],
       [lastX - width*sint/2, lastY + width*cost/2],
       [x - width*sint/2, y + width*cost/2],
-      [x, y]
     ];
+
+    sortVerticesCCW(vertices);
+    return vertices;
+  }
+
+  this.lineTo = function(x, y, width) {
+    // TODO: Could just generate rectangle and rotate?
+    // TODO: Fix rectangles not being adjacent!
+
+    var dx = (x - lastX), dy = (y - lastY);
+    var vertices = buildRectangleVertices(x, y, width);
 
     // CraftyBox expects the position of the center.
     Crafty.e("Box2D").attr({x: dx/2, y: dy/2}).polygon(vertices);
