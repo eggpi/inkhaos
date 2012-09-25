a74ef1e8544382c1da4bad1c93cdaf6f839e8f3c
Guilherme Gonçalves <guilherme.p.gonc@gmail.com>
2012-09-25 13:05:50 -0300

    No more debug drawing.
    We now use Crafty in DOM mode and keep the user's drawings in
    a canvas element.

--- js/inkhaos.js
+++ js/inkhaos.js
@@ -1,47 +1,31 @@
 function initInkhaos() {
   Crafty.init(1280, 768);
-  Crafty.canvas.init();
   Crafty.Box2D.init();
   Crafty.Box2D.gravity = {x: 0, y: 10};
 
   // Use debug drawing mechanism.
-  Crafty.Box2D.debug = true;
+  // Crafty.Box2D.debug = true;
 
-  var canvasElm = Crafty.canvas._canvas;
+  // Create our own canvas in front of the Crafty DOM elements
+  var drawCanvas = $("<canvas width=1280 height=768/>").get(0);
+  $("#cr-stage").append(drawCanvas);
 
-  $(canvasElm).mousedown(function(e) {
+  // Spawn bomb on right mouse click
+  $(drawCanvas).mousedown(function(e) {
     if (e.which == 3) {
-      var bomb = Crafty.e("Box2D").attr({x: e.pageX, y: e.pageY, r: 20, type: "dynamic"});
-
-      // Replace the line above with the following lines to
-      // try our custom drawing mechanism.
-
-      /*
-      var bomb = Crafty.e("Box2D, Canvas").attr({x: e.pageX, y: e.pageY, r: 20, type: "dynamic"});
-      bomb.ready = true; // hack? taken from craftybox examples/box2d.html
-
-      bomb.bind("Draw", function(obj) {
-        obj.ctx.beginPath();
-        var centerX = obj.pos._x + obj.pos._w/2;
-        var centerY = obj.pos._y + obj.pos._h/2;
-
-        obj.ctx.arc(centerX, centerY, obj.pos._w/2, 0, Math.PI*2, true);
-        obj.ctx.closePath();
-        obj.ctx.fill();
-      });
-      */
-
-      return true;
+      Crafty.e("Box2D, DOM")
+            .attr({x: e.pageX, y: e.pageY, r: 20, type: "dynamic"})
+            .css({"background-color": "red", "border-radius": 20});
     }
   });
 
   var lineDrawer, lineBuilder;
   var drawWatcher = new DrawWatcher({
-    canvas: canvasElm,
+    canvas: drawCanvas,
 
     onDrawStart: function(x, y) {
       lineDrawer = new LineDrawer({
-        canvas: canvasElm,
+        canvas: drawCanvas,
         lineColor: "rgb(0,0,0)",
         initialLineWidth: 10.0,
       });
